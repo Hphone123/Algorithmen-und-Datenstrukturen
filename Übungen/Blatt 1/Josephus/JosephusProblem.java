433ead10dc1155de44b2dc6d30e799693f29857c
Hannes Krüger <kruegerh@kd-elektroniksysteme.de>
2024-04-10 19:38:00 +0200

    Josephus partial finish
    Josephus.java finished, self test
    josephus.md begun

--- Übungen/Blatt 1/Josephus/JosephusProblem.java
+++ Übungen/Blatt 1/Josephus/JosephusProblem.java
@@ -6,10 +6,32 @@ public class JosephusProblem {
   // order they were counted out.
   //
   public static Queue<String> solve(String[] persons, int k) {
-    // TODO: implementation
+    return _solve(persons, new Queue<String>(), k, 0);
+  }
+
+  private static Queue<String> _solve (String[] persons, Queue<String> elims, int k, int startEl) {
+    if (persons.length == 0) return elims;
+    int elimIndex = (k - 1 - startEl) % persons.length;
+    elims.enqueue(persons[elimIndex]);
+    String[] _persons = new String[persons.length - 1];
+    int j = 0;
+    for (int i = 0; i < persons.length - 1; i++) {
+      if (i == elimIndex) ++j;
+      _persons[i] = persons[i+j];
+    }
+
+    return _solve(_persons, elims, k, elimIndex);
   }
 
   public static void main(String[] args) {
-    // TODO: test
+    String[] input = {
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6"
+    };
+    System.out.println(solve(input, 4).toString());
   }
 }
--- Übungen/Blatt 1/Josephus/josephus.md
+++ Übungen/Blatt 1/Josephus/josephus.md
@@ -1,9 +1,11 @@
 Lösungsidee
 ---
 
-**TODO**
+Die am Ende eines Reimes ausscheidende Person ist Person k % n vom Anfang der reihe (erste Person, index 0)
+
+Somit ist die Lösung eine Rekursive Methode, die für jede verkleinerte Personenreihe die Ausscheider zurückgibt
 
 Beispiel: Lösung für 13 Personen und 7 Silben im Abzählreim
 ---
 
-**TODO**
+
