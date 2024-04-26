51c7c2b020b8a74961aa52b31c521f7483b285da
Hannes Krüger <kruegerh@kd-elektroniksysteme.de>
2024-04-26 21:29:01 +0200

    MyDList finish

--- Übungen/Blatt 3/MyDList/MyDList.java
+++ Übungen/Blatt 3/MyDList/MyDList.java
@@ -11,16 +11,49 @@ public class MyDList<T> extends DList<T> {
 
 //-----------------------------------------------------------------//
   public void append(MyDList<T> li) {
-    // TODO: append `li` to `this` list
+    MyDList<T> cpy = new MyDList<T>();
+
+    // Create new instance that is not a reference to li
+    for (T t : li) {
+      cpy.push_back(t);
+    }
+    for (T t : cpy) {
+      this.push_back(t);
+    }
+    return;
   }
 
 //-----------------------------------------------------------------//
   public void insert(int n, MyDList<T> li) {
-    // TODO: insert `li` into `this` list at position `n`
+    MyDList<T> cpy = new MyDList<T>();
+
+    // Create a new MyDList instance that is not a pointer copy of li
+    // If li and this are the same instance, the copying will go on forever, because the iterator wont reach the end of the List
+    for (T t : li) {
+      cpy.push_back(t);
+    }
+
+    // Iterate backwards
+    for (T t : cpy.backwards()) {
+      this.insert(n, t);
+    }
   }
 
 //-----------------------------------------------------------------//
   public static void main(String[] args) {
-    // TODO: test your code with appropriate examples
+    MyDList<Integer> list = new MyDList<Integer>();
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(3);
+    list.push_back(4);
+    System.out.println(list.toString());
+
+    list.append(list);
+
+    System.out.println(list.toString());
+
+    list.insert(3, list);
+
+    System.out.println(list.toString());
   }
 }
