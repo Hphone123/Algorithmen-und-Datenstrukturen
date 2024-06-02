ece633cc481ac6b7027ca4ca72b3911f7e6f8b39
Hannes Krüger <kruegerh@kd-elektroniksysteme.de>
2024-06-02 22:12:23 +0200

    Finish Task 8.1

--- Übungen/Blatt 8/BTree/MyBTree.java
+++ Übungen/Blatt 8/BTree/MyBTree.java
@@ -1,27 +1,59 @@
 package BTree;
 
 import aud.BTree;
+import aud.KTreeNode;
 import aud.util.DotViewer;
 
-public class MyBTree {
+public class MyBTree<T extends Comparable<T>> extends BTree<T> {
 
-   BTree<Integer> Tree;
-
-   public MyBTree (Integer [] __values) {
-      Tree = new BTree<>(2);
-
-      DotViewer Window = DotViewer.displayWindow("Amogus" , "Penis");
-      Window.display("null");
-
-      //TODO: Implement
+   public MyBTree(int m) {
+      super(m);
    }
 
    public int getHeight() {
-      //TODO: Implement
-      return 0;
+      return getHeightFromNode(root()); //* Find Maximum Height Below Root Node 
+   }
+
+   public void insertAndViewArray(T [] __data) {
+      DotViewer viewer = DotViewer.displayWindow(this, "MyBTree");
+      viewer.help();
+      for (int i = 0; i < __data.length; i++) {
+         this.insert(__data[i]);
+         viewer.display(this);
+      }
+   }
+
+   public void insertArray (T [] __data) {
+      for (int i = 0; i < __data.length; i++) {
+         this.insert(__data[i]);
+      }
+   }
+
+   //* Recursively Search all Child Nodes to find Maximum Height */
+   private int getHeightFromNode (KTreeNode<T> __node) {
+      if (__node == null) return 0; //* Node is Empty => Height is 0 */
+
+      int maxHeight = 0; //* Maximum Height Below this Node (for all children)
+      for (int i = 0; i < __node.getK(); i++) {
+         if (__node.getChild(i) != null) {
+            int childHeight = getHeightFromNode(__node.getChild(i));
+            if (childHeight > maxHeight) {maxHeight = childHeight;}
+         }
+      }
+
+      return ++maxHeight; //*return Maximum Height for this Node (Plus this Node)
    }
 
    public static void main(String [] args) {
-      MyBTree tree = new MyBTree(null);
+
+      MyBTree<Integer> tree = new MyBTree(2);
+
+      tree.insertAndViewArray(new Integer[]{6, 18, 22, 3, 11, 16, 7, 10, 1, 8, 12});
+
+      MyBTree<Integer> biiiiiiigTree = new MyBTree<Integer>(2);
+      for (int i = 0; i < 1000000; i++) {
+         biiiiiiigTree.insert((int)(Math.random() * 1000000));
+      }
+      System.out.println(biiiiiiigTree.getHeight());
    }
 }
