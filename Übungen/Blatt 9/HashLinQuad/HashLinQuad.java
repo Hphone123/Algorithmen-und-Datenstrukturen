d883108cd78bafb9a887fb14076e4df8f4354f91
Hannes Krüger <kruegerh@kd-elektroniksysteme.de>
2024-06-08 11:26:33 +0200

    Finish HashLinQuad

--- Übungen/Blatt 9/HashLinQuad/HashLinQuad.java
+++ Übungen/Blatt 9/HashLinQuad/HashLinQuad.java
@@ -12,26 +12,77 @@ public class HashLinQuad {
 
   // TODO: Constructor
 
+  public HashLinQuad(int __size) {
+    table_ = new int[__size];
+    size_ = 0;
+    capacity_ = __size;
+
+    for (int i = 0; i < table_.length; i++) {
+      table_[i] = -1; //? -1 signals a spot in the table is free
+    }
+  }
+
   //---------------------------------------------------------------//
   public int addLin(int obj) {
-    // linear probing
-    // return number of collisions
+    int col = 0; //? Colisions
+
+    int hash = Math.floorMod(obj, capacity_); //? Hash function -> ceiling(obj/size_)
+
+    while (table_[hash] != -1) {
+      hash = Math.floorMod(hash + 1, capacity_);
+      col++;
+    }
+
+    table_[hash] = obj;
+    size_++;
+    return col;
   }
 
   //---------------------------------------------------------------//
   public int addQuad(int obj) {
-    // quadratic probing
-    // return number of collisions
+    int col = 0; //? Colisions
+
+    int hash = Math.floorMod(obj, capacity_); //? Hash function -> ceiling(obj/size_)
+    int i = 0;
+    while (table_[hash] != -1) {
+      hash = Math.floorMod(obj + i^2, capacity_);
+      col++; i++;
+    }
+
+    table_[hash] = obj;
+    size_++;
+    return col;
 
   }
 
   //---------------------------------------------------------------//
   public String toString() {
-    // TODO: implementation
+    String res = "Cap.: " + size_ + "/" + capacity_ + " -> [";
+    for (int i = 0; i < table_.length; i++) {
+      if (table_[i] < 0) res += "-";
+      else res += table_[i];
+      res += ",";
+    }
+    res += "]";
+    return res;
   }
 
   //---------------------------------------------------------------//
   public static void main(String[] args) {
-    // TODO: test with hashtable-capacity of 1249 and 1000 random values
+    HashLinQuad hashTaLin = new HashLinQuad(1249);
+    int linCol = 0;
+    for (int i = 0; i < 1000; i++) {
+      linCol += hashTaLin.addLin((int)(Math.random() * 1000));
+    }
+    System.out.println("Collisions with linear probing: " + linCol);
+    System.out.println(hashTaLin.toString());
+  
+    HashLinQuad hashTaQuad = new HashLinQuad(1249);
+    int quadCol = 0;
+    for (int i = 0; i < 1000; i++) {
+      quadCol += hashTaQuad.addLin((int)(Math.random() * 1000));
+    }
+    System.out.println("Collisions with quadratic probing: " + quadCol);
+    System.out.println(hashTaQuad.toString());
   }
 }
