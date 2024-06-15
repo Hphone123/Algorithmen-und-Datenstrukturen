c97eb22230474e68a925b4a8dd51b18dcc39d232
Hannes Krüger <kruegerh@kd-elektroniksysteme.de>
2024-06-15 10:36:00 +0200

    Add inDeg., OutDeg., Adj. functions (Task 10.2)

--- Übungen/Blatt 10/AdjacenseMatrices/AdjMatrix.java
+++ Übungen/Blatt 10/AdjacenseMatrices/AdjMatrix.java
@@ -1,22 +1,47 @@
 package AdjacenseMatrices;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ *  0 0 0
+ *  0 1 0
+ *  1 0 0 <- Ausgehnde Kanten für Knoten 3 (Zeile 3) 
+ *      /\
+ *       |-- Eingehnde Kanten für Knoten 3 (Spalte 3)
+ */     
 
 //---------------------------------------------------------------//
 public class AdjMatrix{
   //-------------------------------------------------------------//
-  public  static int inDegree(int k, int[][] m){
-    //TODO: implement
+  public  static int inDegree(int k, int[][] m) {
+    //! We assume m is a quadratic Matrix, should be checked
+    int sum = 0;
+    for (int i = 0; i < m.length; i++) {
+      if (m[i][k] > 1) ++sum;
+    }
+    return sum;
   }
 
   //-------------------------------------------------------------//
-  public  static int outDegree(int k, int[][] m){
-    //TODO: implement
+  public  static int outDegree(int k, int[][] m) {
+    //! We assume m is a quadratic Matrix, should be checked
+    int sum = 0;
+    for (int i = 0; i < m[k].length; i++) {
+      if (m[k][i] > 1) ++sum;
+    }
+    return sum;
   }
 
   //-------------------------------------------------------------//
   public  static List<Integer> adjacent(int k, int[][] m){
-    //TODO: implement
+    List<Integer> nodes = new ArrayList<Integer>();
+    //! We assume m is a quadratic Matrix, should be checked
+    for (int i = 0; i < m.length; i++) {
+      if (m[k][i] > 1) nodes.add(m[k][i]);
+      if (m[i][k] > 1) nodes.add(m[i][k]);
+    }
+    return nodes;
   }
 
   //-------------------------------------------------------------//
--- Übungen/Blatt 10/Graphs/B10-1_graphs.md
+++ Übungen/Blatt 10/Graphs/B10-1_graphs.md
@@ -1,4 +1,5 @@
-Darstellung von Graphen
+# Darstellung von Graphen
+
 ---
 
 **TODO:** Insert your answer for task 1, 3 and 4!
